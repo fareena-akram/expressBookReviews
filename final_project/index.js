5964bcc5b9c3781ba9dd38ea9a8450470dc52243
fareena-akram <fareenaakram18@gmail.com>
2025-04-21 06:16:16 -0400

    added authntications

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,33 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+let accessToken = jwt.sign({
+    data: password
+}, 'access', { expiresIn: 60 * 60 });
+// Store access token and username in session
+req.session.authorization = {
+    accessToken, username
+}
+if (req.session.authorization) {
+    let token = req.session.authorization['accessToken'];
+    // Verify JWT token
+    jwt.verify(token, "access", (err, user) => {
+        if (!err) {
+            req.username = username;
+            next(); // Proceed to the next middleware
+        } else {
+            return res.status(403).json({ message: "User not authenticated" });
+        }
+    });
+} else {
+    return res.status(403).json({ message: "User not logged in" });
+}
 });
  
 const PORT =5000;
