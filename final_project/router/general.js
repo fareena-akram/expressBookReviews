39ef3016a5a72a801a5e6ddbbeaf8acc51519518
fareena-akram <fareena.akram@ibm.com>
2025-04-22 04:01:14 -0400

    auth user login

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -37,7 +37,6 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-
  res.send(JSON.stringify(books,null,4));
 });
 
@@ -46,7 +45,6 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
     res.send(books[isbn]);
- 
  });
   
 // Get book details based on author
